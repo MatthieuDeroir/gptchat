59b9ae8c9337c81d2bbde97d0d64dfb858ae6b1a
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-08 00:45:06 +0100

    added manifest, icon, service-worker for PWA func

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = [ 'index.html', 'offline.html' ];
+
+const self = this;
+
+// Installation SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// Ecoute pour les requêtes
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+// Activation du SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
